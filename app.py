8a4d6b4050432efaaf5928196c453c799ea7e7cb
Vennessa Hopson <vejreij@microsoft.com>
2023-08-25 15:15:24 -0500

    Correcting huggingface error

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 from apikey import apikey
 
 import streamlit as st
-from langchain.llms import HuggingFacePipeline
+from langchain.llms import HuggingFaceHub
 
 os.environ['HUGGINGFACEHUB_API_TOKEN']=apikey
 
@@ -20,7 +20,7 @@ st.title('🦜️🔗 YouTube GPT Creator')
 prompt=st.text_input('Plug in your prompt here')
 
 # Llms
-llm=HuggingFacePipeline(temperature=0.9 ) #How creative the llm to be
+llm=HuggingFaceHub(repo_id="google/flan-t5-xl",model_kwargs={"temperature":0.9}) 
 
 #Show stuff to the screen if there is a prompt
 if prompt:
