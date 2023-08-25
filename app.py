5727af56b16be5a4233fa4aa5f3563e318a3d205
Vennessa Hopson <vejreij@microsoft.com>
2023-08-25 14:45:20 -0500

    Adding some interface

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+# Bring in deps
 import os
 from apikey import apikey
 
+import streamlit as st
+from langchain.llms import OpenAI
+
 os.environ['HUGGINGFACEHUB_API_TOKEN']=apikey
+
+#App framework
+st.title('🦜️🔗 YouTube GPT Creator')
+prompt=st.text_input('Plug in your prompt here')
