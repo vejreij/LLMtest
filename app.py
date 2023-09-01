bade6d62ed00653352e9f3166c78008e37578a0b
Vennessa Hopson <vejreij@microsoft.com>
2023-09-01 15:58:01 -0500

    Added script template and sequential chain

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import HuggingFaceHub
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 os.environ['HUGGINGFACEHUB_API_TOKEN']=apikey
 
@@ -28,11 +28,18 @@ title_template=PromptTemplate(
     template='write me a yoututbe video title about {topic}'
 )
 
+script_template=PromptTemplate(
+    input_variables=['title'],
+    template='write me a yoututbe video script based on this title TITLE: {title}'
+)
+
 # Llms
 llm=HuggingFaceHub(repo_id="google/flan-t5-xxl",model_kwargs={"temperature":0.9}) 
 title_chain=LLMChain(llm=llm, prompt=title_template,verbose=True)
+script_chain=LLMChain(llm=llm, prompt=script_template,verbose=True)
+sequential_chain=SimpleSequentialChain(chains=[title_chain,script_chain],verbose=True)
 
 #Show stuff to the screen if there is a prompt
 if prompt:
-    response=title_chain.run(topic=prompt)
+    response=sequential_chain.run(topic=prompt)
     st.write(response) #render back to screen
